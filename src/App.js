2bce63a773e57ff0c79c7214e77993f7fc228309
chrisIyer <hellohotbox420@gmail.com>
2023-11-01 19:38:20 -0400

    changes.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { useState, useEffect} from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar'
 import BookmarkPage from './pages/BookmarkPage'
+import AboutPage from './pages/AboutPage'
+import FaqPage from './pages/FaqPage'
+import ResourcePage from './pages/ResourcePage'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div>
+       <BrowserRouter>
       <NavBar />
+
       <Routes>
-        <Route path='/bookmarks' element = {<BookmarkPage />}></Route>
+        <Route path='/bookmarks' element = {<BookmarkPage />}/>
+        <Route path='/about' element = {<AboutPage />}/>
+        <Route path='/faq' element = {<FaqPage />}/>
+        <Route path='/resources' element = {<ResourcePage />}/>
       </Routes>
-      <BookmarkPage />
+      </BrowserRouter>
+
     </div>
   )
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <nav className='nav'>
-      <Link to="/bookmarks">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/faq">FAQs</Link>
-      &nbsp; | &nbsp;
-      <Link to="/resources">Resources Page</Link>
+      <Link to="/bookmarks">Bookmarks Page</Link>
       &nbsp; | &nbsp;
       <Link to="/about">About Us</Link>
+      &nbsp; | &nbsp;
+      <Link to="/faq">FAQs</Link>
+&nbsp; | &nbsp;
+      <Link to="/resources">Resources Page</Link>
+      
+      
     </nav>
   );
 }

b30327a907a107cdbdddce8d97f5659992553fe1
chrisIyer <hellohotbox420@gmail.com>
2023-11-02 13:54:29 -0400

    getting a database error.

--- src/pages/ResourcePage.js
+++ src/pages/ResourcePage.js
@@ -1,24 +1,14 @@
-import React from 'react'
-const resourceList = [
-     {entry: 'Russo, E. B. (2016). Clinical Endocannabinoid Deficiency Reconsidered: Current Research Supports the Theory in Migraine, Fibromyalgia, Irritable Bowel, and Other Treatment-Resistant Syndromes. Cannabis and Cannabinoid Research, 1(1), 154–165',
-     link:'https://doi.org/10.1089/n.2016.0009'},
-     {entry: '', 
-     link: ''},
-     {entry: '', 
-     link: ''},
-     {entry: '', 
-     link: ''},
-     {entry: '', 
-     link: ''},
-     {entry: '', 
-     link: ''}
-]
+
+
 
 const ResourcePage = () => {
 
   return (
     <div>
-     ResourcePage
+     <span><b>Terpines: </b><p>C<sub>5</sub>H<sub>8</sub></p></span>
+   Unsaturated Hydrocarbons produced plants, particularly conifers.
+
+     
      </div>
   )
 }

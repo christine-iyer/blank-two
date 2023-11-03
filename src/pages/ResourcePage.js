04407474271dd3c91e554f4620ce7cf774590cd1
chrisIyer <hellohotbox420@gmail.com>
2023-11-03 13:55:24 -0400

    data added in resource page and as seed json.

--- src/pages/ResourcePage.js
+++ src/pages/ResourcePage.js
@@ -1,14 +1,49 @@
 
-
+import terpines from "../terpines.png"
 
 const ResourcePage = () => {
 
   return (
     <div>
-     <span><b>Terpines: </b><p>C<sub>5</sub>H<sub>8</sub></p></span>
+     <span><b>Terpines: </b><p>(C<sub>5</sub>H<sub>8</sub>)<sub>n</sub> where n represents the number of isoprene units they contain. Isoprene units are the building blocks of terpenes.</p></span>
    Unsaturated Hydrocarbons produced plants, particularly conifers.
+   <ul>
+     <li>Myrcene: Nose - Earthy, hoppy, mango, and lemongrass. Properties - Sleep aid, muscle relaxation. Strains: White Widow, Bubba Kush.</li>
+     <li>Beta-caryophyllene: Nose - Black pepper, cinnamon, cloves, and oregano. Properties - Anti-inflammatory, promotes healthy digestion, and may enhance wound healing. Beta-caryophyllene also has a unique ability to bind directly to CB-2 receptors in the endocannabinoid system. Strains: Girl Scout Cookies, Gorilla Glue #4, and OG Kush (high CBD).</li>
+     <li>Limonene: Nose - Citrus and dill. Properties - Uplifting, productivity support,  possible anti-depressive props due to it effects on neural pathways. Strains: Wedding cake, Do-Si-Do, Lemon Drop. </li>
+     <li>Linalool: Nose - lavender and lilac. Properties - Relaxation, anti-stress, analgesic for body aches in particular, muscle anti-spasmodic. Strains: Lavender, master kush, and abacus.</li>
+     <li>Pinene: Nose - Abundant in conifers, rosemary, and parsley. Properties - Anti-inflammatory, supports memory, antimicrobial, and opens airways. Strains: Kona Gold, Blue Dream, Remedy.</li>
+     <li>Humulene: Nose - Present in ginsing, sage, hops. Properties - Anti-inflammatory, weight-control. Strains: Headband, Sour Diesel, Legendary OG.</li>
+     <li>Terpinolene: Nose - Cumin, nutmeg, apples, and tea. Properties - Tumor growth inhibitor. Cardiovascular support. Strains: Jack Herer.</li>
+     <li>alpha-Bisabolol: Nose - Camomile. Properties - Anti-bacterial, antioxidant, anti-inflammatory  for skin. Strains: ACDC, Pink Kush, Sour Tsunami. </li>
+     <li>Eucalyptol: Nose - Eucalyptus. Properties - antibacterial, antifungal, insect-repelent. Also, may help to lower blood pressure.</li>
+     <li>Geraniol: Nose - Geranium. High quantities in rose and citronella.  Properties -  antimicrobial, antioxidant, antiviral, neuroprotective properties, mosquito repellent.</li>
+     <li>Terpineol: Nose - Properties - : Nose - Properties - Neuroprotective, anti-tumor. </li>
+     <li>Farnesene: Nose - Green apple, ginger, tart fruit. Properties - </li>
+     <li>Borneol: Nose - Rosemary, mint. Properties - Analgesic, anti-inflammatory.</li>
+     <li>Ocimene: Nose - Basil, orchids, and mint. Properties - Fragrance, antioxidant, antimicrobial, and anti-tumor</li>
+     <li>Nerolidol: Nose - WoodyProperties - Cleaning Products, antifungal, antioxidant, antimicrobial, and anti-inflammatory particularly for skin conditions.</li>
+     <li>Guaiol: Nose - Pine. Properties - Indica. Some research supports the natural destruction of cells in lung tumors.</li>
+     <li>Valencene: Nose - Orange, grapefruit, tangerine. Properties - Insect repellent, anti-inflammatory, skin-cancer protagonist. </li>
+     <li>delta-3 Carene: Nose - Cypress, pine, lemon, and musk.  Properties - Assists with neuropathic conditions (fibromyalgia and Alzheimer's). May also assist with broken bones.</li>
+     <li>Phytol: Nose - Jasmine. Strains: Industrial hemp plants. Properties - Pain relief via degradation of GABA. Anti-Stress, Promotes sleep. </li>
+     <li>Sabinene: Nose -  Earthy oak aromas of wine and whiskey, Pepper & Pepper. Properties - Digestive Aid, arthritis, salve for skin rashes, counters muscle atrophy.  </li>
+     <li>Phellandrene: Nose - Eucalyptus, Mint. Citrus Overtones. Properties - Digestive Aid, Depression Aid, Neuropathic Pain. </li>
+     <li>Fenchol: Nose - Fennel, Wood, Citrus. Properties - </li>
+     <li>Menthol: Nose - Menthol. Properties - Analgesic, Anti-bacterial, Anti-inflammatory. Strains: ChemDog, Gelato 45, and Thor’s Hammer.</li>
+     <li>Terpinene: Nose - Earthy. Properties - Anti-Tumor.</li>
+     <li>Isoborneol: Nose - sweet and musky. Properties - Anti-Viral (HSV-1). anti-oxidant, anti-inflammatory, anti-microbial.</li>
+     <li>Cymene: Nose - carrots also orange, tangerine, oregano. Properties - anti-inflammatory and pain relief.</li>
+</ul>
 
-     
+<div>
+     <img src={terpines} alt=""></img>
+</div>
+   Sources:
+   <br></br>
+<a href="https://cannaflower.com/complete-list-terpenes/">A Definitive List of Cannabis-Derives Terpines. </a>
+     <br></br>
+     <a href="https://cannacon.org/15-terpenes-cannabis-explained/">15 terpenes in cannabis explained [Updated 2023]</a>
      </div>
   )
 }
